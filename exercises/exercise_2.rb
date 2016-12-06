ae56c6d620b509e5ec40375526aaf559bd382f77
Richard Zhang <lapislacrima@gmail.com>
2016-12-06 20:50:42 +0000

    changed name of store with id 1 to 'burn a by'

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store3 = Store.find_by(id: 3)
+
+@store1.name = 'burn a by'
+@store1.save
