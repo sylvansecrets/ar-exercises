87011e25c0375d4f34e26f7d829c19a588ae8fa5
Richard Zhang <lapislacrima@gmail.com>
2016-12-06 21:45:28 +0000

    implemented the specified validators, and prompts the user for a store name. \n There is no way for the user to enter a valid store at the moment

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3}
+  has_many :employees, inverse_of: :store
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :must_carry_some_clothes
+
+  def must_carry_some_clothes
+    unless mens_apparel || womens_apparel
+      errors.add(:mens_apparel, 'must carry some clothing')
+      puts ("Must specify what kind of clothing is carried")
+    end
+  end
+
+end
+
+class Employee < ActiveRecord::Base
+  validates :first_name , :last_name, :store, presence: true
+end
+
+puts "Try entering a store name"
+tentative_store = STDIN.gets.strip
+Store.create(name: tentative_store)
+
