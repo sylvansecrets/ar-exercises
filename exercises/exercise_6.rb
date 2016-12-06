44f8aada72365ab810efe263843a2df04dc83490
Richard Zhang <lapislacrima@gmail.com>
2016-12-06 21:23:49 +0000

    creates an employee class that belongs to store, and populates it

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Kram", last_name: "Vi", hourly_rate: -60)
+@store2.employees.create(first_name: "ram", last_name: "rani", hourly_rate: 660)
+@store2.employees.create(first_name: "Khu", last_name: "Vi", hourly_rate: 606)
