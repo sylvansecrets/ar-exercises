9f43d67d74105433a9acca9ac7fdac5f5166095e
Richard Zhang <lapislacrima@gmail.com>
2016-12-06 21:16:47 +0000

    added print for stores with revenue greater than 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -10,5 +10,7 @@ puts "----------"
 # Your code goes here ...
 total_revenue = Store.sum(:annual_revenue)
 store_count = Store.count
+high_count = Store.where('annual_revenue > ?', 1000000).count
 puts "The total revenue for the company in the lower Mainland is #{total_revenue}"
 puts "With the stores averaging a revenue of #{total_revenue/store_count}"
+puts "#{high_count} stores had revenues greater than 1M"
